36a74215054cbad6b7df1c6c0c22ff09a17b229a
Michael Gentili <gentili@mcpnet.ca>
2014-02-09 20:50:08 -0500

    Successfully load, place, and colour an svg

--- src/GameLauncher_cli.js
+++ src/GameLauncher_cli.js
@@ -15,17 +15,31 @@ $(document).ready(function() {
 	$('#screen').bind('cli-load', function(e) {
 		Terminal.setPromptActive(false);
 		Terminal.slowPrint("Booting MCP Kernel V1.0.1\n",
-				function() { Terminal.setWorking(true); window.setTimeout(boot, 1000);});
+				function() { Terminal.setWorking(true); window.setTimeout(boot, 500);});
 	});
 
+	var $hazard = $('<div class="hazard">');
 	function boot() {
 		Terminal.setWorking(false);
 		Terminal.slowPrint(
 			"System Resource Hash: # AF23 4E2E 384B EE25\n"+
 			"Terminal Mode: ASCII\n"+
 			"Connecting to system network",
-			function() { Terminal.setWorking(true); window.setTimeout(done, 1000);});
+			function() { Terminal.setWorking(true); window.setTimeout(hazard, 500);});
 	}
+	
+	function hazard() {
+		Terminal.setWorking(false);
+		Terminal.print ($hazard);
+		jQuery.get('DimentionalTransferWarning.svg',function(data) {
+			var $svg = $(data);
+			$svg.find('path').css("stroke","#1f1");
+			$svg.fadeIn(1000);
+			$hazard.append($svg);
+		},"http");
+		window.setTimeout(hazard, 2000);
+	}
+	
 	
 	function done() {
 		Terminal.setWorking(false);
